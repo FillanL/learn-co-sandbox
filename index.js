fa449f2beda4c7c9e22493dbcee24469e1cda76e
beneficial-kernel-0493 <fillanlee@aol.com>
2019-01-16 01:24:13 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Hello", "Chuck!")
+function say(greeting, firstName) {
+  console.log('I was called!')
+  return `${greeting}, ${firstName}!`
+}
+say("Howdy", "partner")
+say(hi, bob)
+
+const person = {
+  name: "kenny",
+  occuplation: "doc",
+  title: " senior"}
